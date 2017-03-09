a6dd0d033f6879dab679f7ab84b72056f0b9b2e4
andreadelprete <andre.delprete@gmail.com>
2017-03-09 16:51:45 +0100

    Minor

--- include/pininvdyn/tasks/task-base.hpp
+++ include/pininvdyn/tasks/task-base.hpp
@@ -2,16 +2,16 @@
 // Copyright (c) 2017 CNRS
 //
 // This file is part of PinInvDyn
-// pinocchio is free software: you can redistribute it
+// PinInvDyn is free software: you can redistribute it
 // and/or modify it under the terms of the GNU Lesser General Public
 // License as published by the Free Software Foundation, either version
 // 3 of the License, or (at your option) any later version.
-// pinocchio is distributed in the hope that it will be
+// PinInvDyn is distributed in the hope that it will be
 // useful, but WITHOUT ANY WARRANTY; without even the implied warranty
 // of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 // General Lesser Public License for more details. You should have
 // received a copy of the GNU Lesser General Public License along with
-// pinocchio If not, see
+// PinInvDyn If not, see
 // <http://www.gnu.org/licenses/>.
 //
 
@@ -56,10 +56,9 @@ namespace pininvdyn
                                              Data & data) = 0;
       
     protected:
-      
+      std::string m_name;
       /// \brief Reference on the robot model.
       RobotWrapper & m_robot;
-      std::string m_name;
     };
     
   }
