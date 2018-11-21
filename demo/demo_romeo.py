7cdb4e26e292f98d7290995d211e5302e712d63d
Andrea del Prete <adelprete@tuebingen.mpg.de>
2018-11-21 11:50:13 +0100

    Clean up code in demo_romeo.py

--- demo/demo_romeo.py
+++ demo/demo_romeo.py
@@ -1,15 +1,46 @@
 import pinocchio as se3
 import tsid
-
 import numpy as np
-import copy
-
-print ""
-print "Test InvDyn"
-print ""
-
+from numpy.linalg import norm as norm
 import os
 
+import gepetto.corbaserver
+import time
+import commands
+
+np.set_printoptions(precision=3, linewidth=200)
+
+print "".center(100,'#')
+print " Test Task Space Inverse Dynamics ".center(100, '#')
+print "".center(100,'#'), '\n'
+
+lxp = 0.14                          # foot length in positive x direction
+lxn = 0.077                         # foot length in negative x direction
+lyp = 0.069                         # foot length in positive y direction
+lyn = 0.069                         # foot length in negative y direction
+lz = 0.105                          # foot sole height with respect to ankle joint
+mu = 0.3                            # friction coefficient
+fMin = 5.0                          # minimum normal force
+fMax = 1000.0                       # maximum normal force
+rf_frame_name = "RAnkleRoll"        # right foot frame name
+lf_frame_name = "LAnkleRoll"        # left foot frame name
+contactNormal = np.matrix([0., 0., 1.]).T   # direction of the normal to the contact surface
+w_com = 1.0                     # weight of center of mass task
+w_posture = 1e-3                # weight of joint posture task
+w_forceRef = 1e-5               # weight of force regularization task
+w_RF = 1.0                      # weight of right foot motion task
+kp_contact = 30.0               # proportional gain of contact constraint
+kp_com = 30.0                   # proportional gain of center of mass task
+kp_posture = 30.0               # proportional gain of joint posture task
+kp_RF = 30.0                    # proportional gain of right foot motion task
+REMOVE_CONTACT_N = 100          # remove right foot contact constraint after REMOVE_CONTACT_N time steps
+CONTACT_TRANSITION_TIME = 1.0   # duration of the contact transition (to smoothly get a zero contact force before removing a contact constraint)
+DELTA_COM_Y = 0.1               # distance between initial and desired center of mass position in y direction
+DELTA_FOOT_Z = 0.1              # desired elevation of right foot in z direction
+dt = 0.001                      # controller time step
+PRINT_N = 500                   # print every PRINT_N time steps
+DISPLAY_N = 25                  # update robot configuration in viwewer every DISPLAY_N time steps
+N_SIMULATION = 4000             # number of time steps simulated
 
 filename = str(os.path.dirname(os.path.abspath(__file__)))
 path = filename + '/../models/romeo'
@@ -20,46 +51,28 @@ robot = tsid.RobotWrapper(urdf, vector, se3.JointModelFreeFlyer(), False)
 srdf = path + '/srdf/romeo_collision.srdf'
 
 # for gepetto viewer .. but Fix me!!
-robot2 = se3.RobotWrapper(urdf, [path, ], se3.JointModelFreeFlyer())
-import gepetto.corbaserver
-import time
-import commands
+robot_display = se3.RobotWrapper(urdf, [path, ], se3.JointModelFreeFlyer())
+
 l = commands.getstatusoutput("ps aux |grep 'gepetto-gui'|grep -v 'grep'|wc -l")
 if int(l[1]) == 0:
     os.system('gepetto-gui &')
 time.sleep(1)
 cl = gepetto.corbaserver.Client()
 gui = cl.gui
-robot2.initDisplay(loadModel=True)
+robot_display.initDisplay(loadModel=True)
 
 q = se3.getNeutralConfigurationFromSrdf(robot.model(), srdf, False)
 q[2] += 0.84
 v = np.matrix(np.zeros(robot.nv)).transpose()
 
-robot2.display(q)
-
-t = 0.0
-lxp = 0.14
-lxn = 0.077
-lyp = 0.069
-lyn = 0.069
-lz = 0.105
-mu = 0.3
-fMin = 5.0
-fMax = 1000.0
-rf_frame_name = "RAnkleRoll"
-lf_frame_name = "LAnkleRoll"
-contactNormal = np.matrix([0.0, 0.0, 1.0]).transpose()
-w_com = 1.0
-w_posture = 1e-2
-w_forceRef = 1e-5
-kp_contact = 100.0
-kp_com = 30.0
-kp_posture = 30.0
+robot_display.displayCollisions(False)
+robot_display.displayVisuals(True)
+robot_display.display(q)
 
 assert robot.model().existFrame(rf_frame_name)
 assert robot.model().existFrame(lf_frame_name)
 
+t = 0.0                         # time
 invdyn = tsid.InvDyn("tsid", robot, False)
 invdyn.computeProblemData(t, q, v)
 data = invdyn.data()
@@ -91,84 +104,74 @@ postureTask.setKp(kp_posture * np.matrix(np.ones(robot.nv-6)).transpose())
 postureTask.setKd(2.0 * np.sqrt(kp_posture) * np.matrix(np.ones(robot.nv-6)).transpose())
 invdyn.addMotionTask(postureTask, w_posture, 1, 0.0)
 
-dt = 0.001
-PRINT_N = 10
-REMOVE_CONTACT_N = 100
-CONTACT_TRANSITION_TIME = 1.0
-kp_RF = 100.0
-w_RF = 1e3
-max_it = 5000
-rightFootTask = tsid.TaskSE3("task-right-foo", robot, rf_frame_name)
+rightFootTask = tsid.TaskSE3("task-right-foot", robot, rf_frame_name)
 rightFootTask.setKp(kp_RF * np.matrix(np.ones(6)).transpose())
 rightFootTask.setKd(2.0 * np.sqrt(kp_com) * np.matrix(np.ones(6)).transpose())
-H_rf_ref = robot.position(data, robot.model().getJointId(rf_frame_name))
 invdyn.addMotionTask(rightFootTask, w_RF, 1, 0.0)
 
-s = tsid.TrajSample(12, 6)
-H_rf_ref_vec = np.matrix(np.zeros(12)).transpose()
-H_rf_ref_vec[0:3] = H_rf_ref.translation
-for i in range(0,3):
-    H_rf_ref_vec[3*i+3: 3*i+6] = H_rf_ref.rotation[:, i]
-s.pos(H_rf_ref_vec)
-rightFootTask.setReference(s)
+H_rf_ref.translation += np.matrix([0., 0., DELTA_FOOT_Z]).T
+rightFootTraj = tsid.TrajSE3Constant("traj-right-foot", H_rf_ref)
 
 com_ref = robot.com(data)
-com_ref[1] += 0.1
+com_ref[1] += DELTA_COM_Y
 trajCom = tsid.TrajEuclidianConstant("traj_com", com_ref)
-sampleCom = tsid.TrajSample(3)
 
 q_ref = q[7:]
 trajPosture = tsid.TrajEuclidianConstant("traj_joint", q_ref)
-samplePosture = tsid.TrajSample(robot.nv-6)
 
 solver = tsid.SolverQuadProg("qp solver")
 solver.resize(invdyn.nVar, invdyn.nEq, invdyn.nIn)
 
-tau_old = np.matrix(np.zeros(robot.nv-6)).transpose()
-
-for i in range(0, max_it):
+for i in range(0, N_SIMULATION):
+    time_start = time.time()
+    
     if i == REMOVE_CONTACT_N:
-        print "Start breaking contact right foot"
+        print "\nTime %.3f Start breaking contact %s\n"%(t, contactRF.name)
         invdyn.removeRigidContact(contactRF.name, CONTACT_TRANSITION_TIME)
 
     sampleCom = trajCom.computeNext()
     comTask.setReference(sampleCom)
     samplePosture = trajPosture.computeNext()
     postureTask.setReference(samplePosture)
+    sampleRightFoot = rightFootTraj.computeNext()
+    rightFootTask.setReference(sampleRightFoot)
 
     HQPData = invdyn.computeProblemData(t, q, v)
     if i == 0:
         HQPData.print_all()
 
-    from numpy.linalg import norm as norm
-
     sol = solver.solve(HQPData)
     tau = invdyn.getActuatorForces(sol)
     dv = invdyn.getAccelerations(sol)
 
-    if i > 0:
-        #assert norm(tau-tau_old) < 2e1
-        tau_old = tau
-        if i%PRINT_N == 0:
-            print "Time ", i
-            if invdyn.checkContact(contactRF.name, sol):
-                f = invdyn.getContactForce(contactRF.name, sol)
-                print "   ", contactRF.name, "force: ", contactRF.getNormalForce(f)
+    if i%PRINT_N == 0:
+        print "Time %.3f"%(t)
+        if invdyn.checkContact(contactRF.name, sol):
+            f = invdyn.getContactForce(contactRF.name, sol)
+            print "\tnormal force %s: %.1f"%(contactRF.name.ljust(20,'.'),contactRF.getNormalForce(f))
 
-            if invdyn.checkContact(contactLF.name, sol):
-                f = invdyn.getContactForce(contactLF.name, sol)
-                print "   ", contactLF.name, "force: ", contactLF.getNormalForce(f)
+        if invdyn.checkContact(contactLF.name, sol):
+            f = invdyn.getContactForce(contactLF.name, sol)
+            print "\tnormal force %s: %.1f"%(contactLF.name.ljust(20,'.'),contactLF.getNormalForce(f))
 
-            print "   ", comTask.name, " err:", norm(comTask.position_error, 2)
-            print "   ", "v: ", norm(v, 2), "dv: ", norm(dv)
-            robot2.display(q)
+        print "\ttracking err %s: %.3f"%(comTask.name.ljust(20,'.'),       norm(comTask.position_error, 2))
+        print "\ttracking err %s: %.3f"%(rightFootTask.name.ljust(20,'.'), norm(rightFootTask.position_error, 2))
+        print "\t||v||: %.3f\t ||dv||: %.3f"%(norm(v, 2), norm(dv))
 
+    v_mean = v + 0.5*dt*dv
     v += dt*dv
-    q = se3.integrate(robot.model(), q, dt * v)
+    q = se3.integrate(robot.model(), q, dt*v_mean)
     t += dt
+    
+    if i%DISPLAY_N == 0:
+        robot_display.display(q)
 
+    time_spent = time.time() - time_start
+    if(time_spent < dt):
+        time.sleep(dt-time_spent)
+        
     assert norm(dv) < 1e6
     assert norm(v) < 1e6
 
-print "Final COM Position", robot.com(invdyn.data()).transpose()
-print "Desired COM Position", com_ref.transpose()
+print "\nFinal COM Position  ", robot.com(invdyn.data()).T
+print "Desired COM Position", com_ref.T
