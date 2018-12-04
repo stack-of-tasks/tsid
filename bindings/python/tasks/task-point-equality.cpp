96433843a1a9f1589023c983cf6171f1a75b5070
Julian Viereck <jviereck@tuebingen.mpg.de>
2018-12-04 17:45:08 +0100

    Expose task-point-equality to python

--- bindings/python/tasks/task-point-equality.cpp
+++ bindings/python/tasks/task-point-equality.cpp
@@ -15,33 +15,16 @@
 // <http://www.gnu.org/licenses/>.
 //
 
-#ifndef __tsid_python_expose_tasks_hpp__
-#define __tsid_python_expose_tasks_hpp__
-
-#include "tsid/bindings/python/tasks/task-com-equality.hpp"
-#include "tsid/bindings/python/tasks/task-se3-equality.hpp"
 #include "tsid/bindings/python/tasks/task-point-equality.hpp"
-#include "tsid/bindings/python/tasks/task-joint-posture.hpp"
-
-
+#include "tsid/bindings/python/tasks/expose-tasks.hpp"
 
 namespace tsid
 {
   namespace python
   {
-    void exposeTaskComEquality();
-    void exposeTaskSE3Equality();
-    void exposeTaskPointEquality();
-    void exposeTaskJointPosture();
-
-    inline void exposeTasks()
+    void exposeTaskPointEquality()
     {
-      exposeTaskComEquality();
-      exposeTaskSE3Equality();
-      exposeTaskPointEquality();
-      exposeTaskJointPosture();
+      TaskPointEqualityPythonVisitor<tsid::tasks::TaskPointEquality>::expose("TaskPointEquality");
     }
-    
-  } // namespace python
-} // namespace tsid
-#endif // ifndef __tsid_python_expose_tasks_hpp__
+  }
+}
