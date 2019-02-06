2ea05933991648d158ea69300ba3bd00fb4a6e68
Andrea Del Prete <andre.delprete@gmail.com>
2019-02-06 03:08:06 -0800

    Add deprecation macro

---
+++ include/tsid/deprecation.hpp
@@ -0,0 +1,30 @@
+//
+// Copyright (c) 2019 University of Trento
+//
+// This file is part of tsid
+// tsid is free software: you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation, either version
+// 3 of the License, or (at your option) any later version.
+// tsid is distributed in the hope that it will be
+// useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+// of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// General Lesser Public License for more details. You should have
+// received a copy of the GNU Lesser General Public License along with
+// tsid If not, see
+// <http://www.gnu.org/licenses/>.
+//
+
+#ifndef _tsid_lib_deprecation_hpp
+#define _tsid_lib_deprecation_hpp
+
+#if defined(__GNUC__) || defined(__clang__)
+#define DEPRECATED __attribute__((deprecated))
+#elif defined(_MSC_VER)
+#define DEPRECATED __declspec(deprecated)
+#else
+#pragma message("WARNING: You need to implement DEPRECATED for this compiler")
+#define DEPRECATED
+#endif
+
+#endif //_tsid_lib_deprecation_hpp
