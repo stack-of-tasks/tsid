6f37e4dc4dc14599ef363683fc8fd8816a3c2218
Noëlie Ramuzat <noelie.ramuzat@laas.fr>
2021-07-21 13:11:57 +0200

    [ForceTask] First draft

--- include/tsid/tasks/task-contact-force-equality.hpp
+++ include/tsid/tasks/task-contact-force-equality.hpp
@@ -15,15 +15,83 @@
 // <http://www.gnu.org/licenses/>.
 //
 
-#ifndef __invdyn_tasks_fwd_hpp__
-#define __invdyn_tasks_fwd_hpp__
+#ifndef __invdyn_task_contact_force_equality_hpp__
+#define __invdyn_task_contact_force_equality_hpp__
+
+#include "tsid/math/fwd.hpp"
+#include "tsid/tasks/task-contact-force.hpp"
+#include "tsid/trajectories/trajectory-base.hpp"
+#include "tsid/math/constraint-equality.hpp"
+#include "tsid/formulations/inverse-dynamics-formulation-base.hpp"
 
 namespace tsid
 {
   namespace tasks
   {
-    
+
+    class TaskContactForceEquality : public TaskContactForce
+    {
+    public:
+      EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+      typedef math::Index Index;
+      typedef trajectories::TrajectorySample TrajectorySample;
+      typedef math::Vector Vector;
+      typedef math::Vector6 Vector6;
+      typedef math::ConstraintEquality ConstraintEquality;
+      typedef pinocchio::SE3 SE3;
+
+      TaskContactForceEquality(const std::string & name,
+                      		   RobotWrapper & robot,
+                      		   const std::string & contactName="");
+
+      void setContactList(const std::vector<std::shared_ptr<ContactLevel> >  *contacts);
+
+      int dim() const;
+
+      virtual const std::string& getAssociatedContactName();
+      virtual void setAssociatedContactName(const std::string & contactName);
+
+      const ConstraintBase & compute(const double t,
+                                     ConstRefVector q,
+                                     ConstRefVector v,
+                                     Data & data);
+
+      const ConstraintBase & compute(const double t,
+                                     ConstRefVector q,
+                                     ConstRefVector v,
+                                     Data & data,
+                                     const std::vector<std::shared_ptr<ContactLevel> >  *contacts);
+
+      const ConstraintBase & getConstraint() const;
+
+      void setReference(TrajectorySample & ref);
+      const TrajectorySample & getReference() const;
+
+      void setExternalForce(TrajectorySample & f_ext);
+      const TrajectorySample & getExternalForce() const;
+
+      const Vector & Kp() const;
+      const Vector & Kd() const;
+      const Vector & Ki() const;
+      void Kp(ConstRefVector Kp);
+      void Kd(ConstRefVector Kp);
+      void Ki(ConstRefVector Ki);
+
+    protected:
+      const std::vector<std::shared_ptr<ContactLevel> > *m_contacts;
+      std::string m_contact_name; // an empty string
+      ConstraintEquality m_constraint;
+      TrajectorySample m_ref;  // reference Force in world frame
+      TrajectorySample m_fext;     // external force in the world frame
+      Vector6 m_forceIntegralError;
+	  Vector m_Kp;
+      Vector m_Kd;
+      Vector m_Ki;
+      double m_dt;
+    };
+
   }
 }
 
-#endif // ifndef __invdyn_tasks_fwd_hpp__
+#endif // ifndef __invdyn_task_contact_force_equality_hpp__
