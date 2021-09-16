bce8eb8bacf656c39275809db4f52af5b4111543
Noëlie RAMUZAT <ramuzat.noelie@gmail.com>
2021-09-16 11:13:17 +0200

    Add implementation of TaskContactForceEquality (#141)
    * [ForceTask] Add bindings and set task for contactPoint (3D), use Contact6D, put dt as an input in the constructor & add feedforward, add leak_rate parameter, change constructor & methods to take the contact
    
    Authored-by: Noëlie Ramuzat <noelie.ramuzat@laas.fr>

--- include/tsid/tasks/task-contact-force-equality.hpp
+++ include/tsid/tasks/task-contact-force-equality.hpp
@@ -15,15 +15,89 @@
 // <http://www.gnu.org/licenses/>.
 //
 
-#ifndef __invdyn_tasks_fwd_hpp__
-#define __invdyn_tasks_fwd_hpp__
+#ifndef __invdyn_task_contact_force_equality_hpp__
+#define __invdyn_task_contact_force_equality_hpp__
+
+#include "tsid/math/fwd.hpp"
+#include "tsid/tasks/task-contact-force.hpp"
+#include "tsid/trajectories/trajectory-base.hpp"
+#include "tsid/math/constraint-equality.hpp"
+#include "tsid/contacts/contact-base.hpp"
 
 namespace tsid
 {
   namespace tasks
   {
-    
+
+    class TaskContactForceEquality : public TaskContactForce
+    {
+    public:
+      EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+
+      typedef math::Index Index;
+      typedef trajectories::TrajectorySample TrajectorySample;
+      typedef math::Vector Vector;
+      typedef math::Vector6 Vector6;
+      typedef math::Vector3 Vector3;
+      typedef math::ConstraintEquality ConstraintEquality;
+      typedef pinocchio::SE3 SE3;
+
+      TaskContactForceEquality(const std::string & name,
+                      		     RobotWrapper & robot,
+                               const double dt,
+                      		     contacts::ContactBase & contact);
+
+      int dim() const;
+
+      virtual const std::string& getAssociatedContactName();
+      virtual const contacts::ContactBase& getAssociatedContact();
+      void setAssociatedContact(contacts::ContactBase & contact);
+
+      // Task expressed as a PID between the reference force and the external one
+      const ConstraintBase & compute(const double t,
+                                     ConstRefVector q,
+                                     ConstRefVector v,
+                                     Data & data);
+
+      const ConstraintBase & compute(const double t,
+                                     ConstRefVector q,
+                                     ConstRefVector v,
+                                     Data & data,
+                                     const std::vector<std::shared_ptr<ContactLevel> >  *contacts);
+
+      const ConstraintBase & getConstraint() const;
+
+      void setReference(TrajectorySample & ref);
+      const TrajectorySample & getReference() const;
+
+      void setExternalForce(TrajectorySample & f_ext);
+      const TrajectorySample & getExternalForce() const;
+
+      const Vector & Kp() const;
+      const Vector & Kd() const;
+      const Vector & Ki() const;
+      const double & getLeakRate() const;
+      void Kp(ConstRefVector Kp);
+      void Kd(ConstRefVector Kp);
+      void Ki(ConstRefVector Ki);
+      void setLeakRate(double leak);
+
+    protected:
+      // contact associated to the force task
+      contacts::ContactBase * m_contact;
+      std::string m_contact_name; // the associated contact name or an empty string
+      ConstraintEquality m_constraint;
+      TrajectorySample m_ref;      // reference Force 6D to follow 
+      TrajectorySample m_fext;     // external Force 6D in the same frame than the ref
+      Vector m_forceIntegralError; // Integral error of the PID
+	    Vector m_Kp;
+      Vector m_Kd;
+      Vector m_Ki;
+      double m_dt;
+      double m_leak_rate;
+    };
+
   }
 }
 
-#endif // ifndef __invdyn_tasks_fwd_hpp__
+#endif // ifndef __invdyn_task_contact_force_equality_hpp__
--- src/tasks/task-contact-force-equality.cpp
+++ src/tasks/task-contact-force-equality.cpp
@@ -17,12 +17,135 @@
 
 #include <Eigen/Dense>
 #include <pinocchio/multibody/model.hpp>
+#include "tsid/tasks/task-contact-force-equality.hpp"
 
-namespace tsid
+namespace tsid {
+namespace tasks {
+
+using namespace tsid::math;
+using namespace std;
+
+TaskContactForceEquality::TaskContactForceEquality(const std::string & name, RobotWrapper & robot,
+                                                   const double dt, contacts::ContactBase & contact):
+  TaskContactForce(name, robot),
+  m_contact(&contact),
+  m_constraint(name, 6, 12),  
+  m_ref(6,6),
+  m_fext(6,6) {
+  m_forceIntegralError = Vector::Zero(6);
+  m_dt = dt;
+  m_leak_rate = 0.05;
+  m_contact_name = m_contact->name();
+}
+
+int TaskContactForceEquality::dim() const {
+  return 6;
+}
+
+const Vector & TaskContactForceEquality::Kp() const { return m_Kp; }
+const Vector & TaskContactForceEquality::Kd() const { return m_Kd; }
+const Vector & TaskContactForceEquality::Ki() const { return m_Ki; }
+const double & TaskContactForceEquality::getLeakRate() const { return m_leak_rate; }
+
+void TaskContactForceEquality::Kp(ConstRefVector Kp)
 {
-  namespace tasks
-  {
-    
-    
+  assert(Kp.size()==6);
+  m_Kp = Kp;
+}
+
+void TaskContactForceEquality::Kd(ConstRefVector Kd)
+{
+  assert(Kd.size()==6);
+  m_Kd = Kd;
+}
+
+void TaskContactForceEquality::Ki(ConstRefVector Ki)
+{
+  assert(Ki.size()==6);
+  m_Ki = Ki;
+}
+
+void TaskContactForceEquality::setLeakRate(double leak)
+{
+  m_leak_rate = leak;
+}
+
+const std::string& TaskContactForceEquality::getAssociatedContactName() {
+  return m_contact_name;
+}
+
+const contacts::ContactBase& TaskContactForceEquality::getAssociatedContact() {
+  return *m_contact;
+}
+
+void TaskContactForceEquality::setAssociatedContact(contacts::ContactBase & contact) {
+  m_contact = &contact;
+  m_contact_name = m_contact->name();
+}
+
+void TaskContactForceEquality::setReference(TrajectorySample & ref) {
+  m_ref = ref;
+}
+
+const TaskContactForceEquality::TrajectorySample & TaskContactForceEquality::getReference() const {
+  return m_ref;
+}
+
+void TaskContactForceEquality::setExternalForce(TrajectorySample & f_ext) {
+  m_fext = f_ext;
+}
+
+const TaskContactForceEquality::TrajectorySample & TaskContactForceEquality::getExternalForce() const {
+  return m_fext;
+}
+
+
+const ConstraintBase & TaskContactForceEquality::compute(const double t,
+                                                         ConstRefVector q,
+                                                         ConstRefVector v,
+                                                         Data & data,
+                                                         const std::vector<std::shared_ptr<ContactLevel> >  *contacts) {
+  bool contactFound = false;
+  if (m_contact_name != "") {
+    // look if the associated contact is in the list of contact
+    for(auto cl : *contacts) {
+      if (m_contact_name == cl->contact.name()) {
+        contactFound = true;
+        break;
+      }
+    }
+  } else {
+    std::cout << "[TaskContactForceEquality] ERROR: Contact name empty" << std::endl;
+    return m_constraint;
   }
+  if (!contactFound) {
+    std::cout << "[TaskContactForceEquality] ERROR: Contact name not in the list of contact in the formulation pb" << std::endl;
+    return m_constraint;
+  }
+  return compute(t, q, v, data);
+}
+
+const ConstraintBase & TaskContactForceEquality::compute(const double,
+                                                         ConstRefVector,
+                                                         ConstRefVector,
+                                                         Data & data) {
+
+  auto& M = m_constraint.matrix();
+  M = m_contact->getForceGeneratorMatrix(); // 6x12 for a 6d contact
+
+  Vector forceError = m_ref.getValue() - m_fext.getValue();
+  Vector f_ref = m_ref.getValue() + m_Kp.cwiseProduct(forceError) + m_Kd.cwiseProduct(m_ref.getDerivative() - m_fext.getDerivative()) 
+                 + m_Ki.cwiseProduct(m_forceIntegralError);
+  m_constraint.vector() = f_ref;
+
+  m_forceIntegralError += (forceError - m_leak_rate * m_forceIntegralError) * m_dt;
+
+  return m_constraint;
+}
+
+const ConstraintBase & TaskContactForceEquality::getConstraint() const {
+  return m_constraint;
+}
+
+}
 }
