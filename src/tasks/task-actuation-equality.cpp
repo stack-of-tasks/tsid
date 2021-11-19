fced15215c0cebe2d7e7c2e0967b1c50d9a6628a
paulinejmaurice <31210176+paulinejmaurice@users.noreply.github.com>
2021-11-19 22:47:46 +0100

    Task Actuation Equality (#150)
    Add task actuation equality (by pauline.maurice@loria.fr)

--- src/tasks/task-actuation-equality.cpp
+++ src/tasks/task-actuation-equality.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2017 CNRS
+// Copyright (c) 2021 CNRS INRIA LORIA
 //
 // This file is part of tsid
 // tsid is free software: you can redistribute it
@@ -15,11 +15,106 @@
 // <http://www.gnu.org/licenses/>.
 //
 
+#include <tsid/tasks/task-actuation-equality.hpp>
+#include "tsid/robots/robot-wrapper.hpp"
+
 namespace tsid
 {
   namespace tasks
   {
-    
-    
+    using namespace math;
+    using namespace pinocchio;
+
+    TaskActuationEquality::TaskActuationEquality(const std::string & name,
+                                             RobotWrapper & robot):
+     TaskActuation(name, robot),
+     m_constraint(name, robot.na(), robot.na())
+   {
+      m_ref = Vector::Zero(robot.na());
+      m_weights = Vector::Ones(robot.na());
+      Vector m = Vector::Ones(robot.na());
+      mask(m);
+    }
+
+    const Vector & TaskActuationEquality::mask() const
+    {
+      return m_mask;
+    }
+
+    void TaskActuationEquality::mask(const Vector & m)
+    {
+      assert(m.size()==m_robot.na());
+      m_mask = m;
+
+      const Vector::Index dim = static_cast<Vector::Index>(m.sum());
+      Matrix S = Matrix::Zero(dim, m_robot.na());
+      m_activeAxes.resize(dim);
+      unsigned int j=0;
+      for(unsigned int i=0; i<m.size(); i++)
+        if(m(i)!=0.0)
+        {
+          assert(m(i)==1.0);
+          S(j,i) = m_weights(i);
+          m_activeAxes(j) = i;
+          j++;
+        }
+      m_constraint.resize((unsigned int)dim, m_robot.na());
+      m_constraint.setMatrix(S);
+
+      for(unsigned int i=0; i<m_activeAxes.size(); i++)
+        m_constraint.vector()(i) = m_ref(m_activeAxes(i)) * m_weights(m_activeAxes(i));
+    }
+
+    int TaskActuationEquality::dim() const
+    {
+      return (int)m_mask.sum();
+    }
+
+    // Reference should be the same size as robot.na(), even if a mask is used (masked dof values will just be ignored)
+    void TaskActuationEquality::setReference(ConstRefVector ref)
+    {
+      assert(ref.size()==m_robot.na());
+      m_ref = ref;
+
+      for(unsigned int i=0; i<m_activeAxes.size(); i++)
+        m_constraint.vector()(i) = m_ref(m_activeAxes(i)) * m_weights(m_activeAxes(i));
+    }
+
+    const Vector & TaskActuationEquality::getReference() const
+    {
+      return m_ref;
+    }
+
+    // Weighting vector should be the same size as robot.na(), even if a mask is used (masked dof values will just be ignored)
+    void TaskActuationEquality::setWeightVector(ConstRefVector weights)
+    {
+      assert(weights.size()==m_robot.na());
+      m_weights = weights;
+
+      for(unsigned int i=0; i<m_activeAxes.size(); i++)
+      {
+        m_constraint.matrix()(i,m_activeAxes(i)) = m_weights(m_activeAxes(i));
+        m_constraint.vector()(i) = m_ref(m_activeAxes(i)) * m_weights(m_activeAxes(i));
+      }
+    }
+
+    const Vector & TaskActuationEquality::getWeightVector() const
+    {
+      return m_weights;
+    }
+
+    const ConstraintBase & TaskActuationEquality::getConstraint() const
+    {
+      return m_constraint;
+    }
+
+    const ConstraintBase & TaskActuationEquality::compute(const double ,
+                                                        ConstRefVector ,
+                                                        ConstRefVector ,
+                                                        Data & )
+    {
+      return m_constraint;
+    }
+
   }
 }
