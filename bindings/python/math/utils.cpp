390ea4bbe097fec99bdfaa7ea18325eb8485d15b
EtienneAr <earlaud@pl346-pro.paris.inria.fr-1>
2022-11-14 17:41:00 +0100

    Add more convenient bindings to SE3ToVector and vectorToSE3

--- bindings/python/math/utils.cpp
+++ bindings/python/math/utils.cpp
@@ -24,13 +24,29 @@ namespace tsid
 {
   namespace python
   {
+    static math::Vector SE3ToVector_1(const pinocchio::SE3 & M)
+    {
+      math::Vector res(12);
+      math::SE3ToVector(M, res);
+      return res;
+    }
+
+    static pinocchio::SE3 vectorToSE3_1(math::RefVector vec)
+    {
+      pinocchio::SE3 res;
+      math::vectorToSE3(vec, res);
+      return res;
+    }
+
     void exposeMathUtils()
     {
         namespace bp = boost::python;
         using namespace math;
 
         bp::def("SE3ToVector", &SE3ToVector, bp::args("M", "vec"), "Convert the input SE3 object M to a 12D vector of floats [X,Y,Z,R11,R12,R13,R14,...] vec");
+        bp::def("SE3ToVector", &SE3ToVector_1, bp::args("M"), "Convert the input SE3 object M to a 12D vector of floats [X,Y,Z,R11,R12,R13,R14,...] and return the vector");
         bp::def("vectorToSE3", &vectorToSE3, bp::args("vec", "M"), "Convert the input 12D vector of floats [X,Y,Z,R11,R12,R13,R14,...] vec to a SE3 object M");
+        bp::def("vectorToSE3", &vectorToSE3_1, bp::args("vec"), "Convert the input 12D vector of floats [X,Y,Z,R11,R12,R13,R14,...] vec to a SE3 object and return the SE3 object");
     }
   }
 }
