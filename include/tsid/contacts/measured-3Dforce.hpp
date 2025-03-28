6b470bbcdf55de45fc3b79fbdf2a9f1bd2e7b660
Alexandre <alexandre.oliveira-souza@inria.fr>
2025-03-28 15:08:33 +0000

    adding back deprecated files

---
+++ include/tsid/contacts/measured-3Dforce.hpp
@@ -0,0 +1,12 @@
+#ifndef TSID_CONTACTS_MEASURED_3DFORCE_HPP
+#define TSID_CONTACTS_MEASURED_3DFORCE_HPP
+
+#include "tsid/contacts/measured-3d-force.hpp"
+
+#ifdef _MSC_VER
+#pragma message("TSID DEPRECATED: Please update your includes from 'measured-3Dforce.hpp' to 'measured-3d-force.hpp'")
+#else
+#warning "TSID DEPRECATED: Please update your includes from 'measured-3Dforce.hpp' to 'measured-3d-force.hpp'"
+#endif
+
+#endif // TSID_CONTACTS_MEASURED_3DFORCE_HPP
---
+++ include/tsid/contacts/measured-6Dwrench.hpp
@@ -0,0 +1,12 @@
+#ifndef TSID_CONTACTS_MEASURED_6DWRENCH_HPP
+#define TSID_CONTACTS_MEASURED_6DWRENCH_HPP
+
+#include "tsid/contacts/measured-6d-wrench.hpp"
+
+#ifdef _MSC_VER
+#pragma message("TSID DEPRECATED: Please update your includes from 'measured-6Dwrench.hpp' to 'measured-6d-wrench.hpp'")
+#else
+#warning "TSID DEPRECATED: Please update your includes from 'measured-6Dwrench.hpp' to 'measured-6d-wrench.hpp'"
+#endif
+
+#endif // TSID_CONTACTS_MEASURED_6DWRENCH_HPP
