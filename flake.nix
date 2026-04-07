1571368cd6de12640c6466cd6c8f556a7e01f751
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-07 10:53:55 +0200

    sync with gepetto/nix template

--- flake.nix
+++ flake.nix
@@ -2,31 +2,25 @@
   description = "Efficient Task Space Inverse Dynamics (TSID) based on Pinocchio.";
 
   inputs = {
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    gepetto.url = "github:gepetto/nix";
+    flake-parts.follows = "gepetto/flake-parts";
+    systems.follows = "gepetto/systems";
   };
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = inputs.nixpkgs.lib.systems.flakeExposed;
-      perSystem =
-        { pkgs, self', ... }:
-        {
-          apps.default = {
-            type = "app";
-            program = pkgs.python3.withPackages (_: [ self'.packages.default ]);
-          };
-          devShells.default = pkgs.mkShell {
-            inputsFrom = [ self'.packages.default ];
-            packages = [ (pkgs.python3.withPackages (p: [ p.tomlkit ])) ]; # for "make release"
-          };
-          packages = {
-            default = self'.packages.tsid;
-            tsid = pkgs.python3Packages.tsid.overrideAttrs (_: {
-              src = pkgs.lib.fileset.toSource {
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, ... }:
+      {
+        systems = import inputs.systems;
+        imports = [
+          inputs.gepetto.flakeModule
+          {
+            flakoboros.extraDevPyPackages = [ "tsid" ];
+            flakoboros.overrideAttrs.tsid = _: {
+              src = lib.fileset.toSource {
                 root = ./.;
-                fileset = pkgs.lib.fileset.unions [
+                fileset = lib.fileset.unions [
                   ./bindings
                   ./CMakeLists.txt
                   ./doc
@@ -37,8 +31,9 @@
                   ./tests
                 ];
               };
-            });
-          };
-        };
-    };
+            };
+          }
+        ];
+      }
+    );
 }
