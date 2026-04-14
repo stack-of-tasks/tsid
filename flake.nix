4213580574e840dc77ff5beabc2821c4dc1c66e6
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-14 10:56:15 +0200

    nix: switch to mkflakoboros

--- flake.nix
+++ flake.nix
@@ -1,39 +1,29 @@
 {
   description = "Efficient Task Space Inverse Dynamics (TSID) based on Pinocchio.";
 
-  inputs = {
-    gepetto.url = "github:gepetto/nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    systems.follows = "gepetto/systems";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.gepetto.lib.mkFlakoboros (
       { lib, ... }:
       {
-        systems = import inputs.systems;
-        imports = [
-          inputs.gepetto.flakeModule
-          {
-            flakoboros.extraDevPyPackages = [ "tsid" ];
-            flakoboros.overrideAttrs.tsid = _: {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./bindings
-                  ./CMakeLists.txt
-                  ./doc
-                  ./include
-                  ./models
-                  ./package.xml
-                  ./src
-                  ./tests
-                ];
-              };
-            };
-          }
-        ];
+        extraDevPyPackages = [ "tsid" ];
+        overrideAttrs.tsid = {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./bindings
+              ./CMakeLists.txt
+              ./doc
+              ./include
+              ./models
+              ./package.xml
+              ./src
+              ./tests
+            ];
+          };
+        };
       }
     );
 }
