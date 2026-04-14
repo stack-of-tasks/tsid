cb49a51bbb174a5a2a96378563dc7fd6fa41cdcc
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-14 13:15:01 +0200

    nix: switch to mkFlakoboros

--- flake.nix
+++ flake.nix
@@ -5,25 +5,27 @@
 
   outputs =
     inputs:
-    inputs.gepetto.lib.mkFlakoboros (
+    inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
         extraDevPyPackages = [ "tsid" ];
-        overrideAttrs.tsid = {
-          src = lib.fileset.toSource {
-            root = ./.;
-            fileset = lib.fileset.unions [
-              ./bindings
-              ./CMakeLists.txt
-              ./doc
-              ./include
-              ./models
-              ./package.xml
-              ./src
-              ./tests
-            ];
+        overrideAttrs.tsid =
+          { ... }:
+          {
+            src = lib.fileset.toSource {
+              root = ./.;
+              fileset = lib.fileset.unions [
+                ./bindings
+                ./CMakeLists.txt
+                ./doc
+                ./include
+                ./models
+                ./package.xml
+                ./src
+                ./tests
+              ];
+            };
           };
-        };
       }
     );
 }
